1199a65e9be8ba64a825337922afb0a904193972
Pedro Coelho <pedro_junior37@hotmail.com>
2024-01-06 05:23:52 -0300

    Adicionar arquivos de projeto.

---
+++ BeeCrownd/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program {
+    public static void Main(string[] args) {
+        int funcionario = int.Parse(Console.ReadLine());
+        int hora = int.Parse(Console.ReadLine());
+        double salario = double.Parse(Console.ReadLine());
+
+        double salarioFinal = hora * salario;
+        //double salarioFinal = Math.Round(hora * salario, 2);
+        Console.WriteLine("NUMBER = " + funcionario);
+        Console.Write("SALARY = U$ " + salarioFinal.ToString("F2"));
+    }
+}
